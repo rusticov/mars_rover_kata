a3e55d18ea7ad786db1bb023a1438a5061005ff5
Rusticov <knightdestruction@pm.me>
2024-12-09 22:42:49 +0000

    refactor: Move Direction into its own file

---
+++ src/direction.rs
@@ -0,0 +1,26 @@
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    pub fn char(&self) -> char {
+        match self {
+            Direction::North => 'N',
+            Direction::East => 'E',
+            Direction::South => 'S',
+            Direction::West => 'W',
+        }
+    }
+
+    pub fn turn_right(&self) -> Direction {
+        match self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod direction;
+
+use direction::Direction;
+
 pub struct Rover {
     direction: Direction,
 }
@@ -15,33 +19,6 @@ impl Rover {
     pub fn new() -> Rover {
         Rover {
             direction: Direction::North,
-        }
-    }
-}
-
-enum Direction {
-    North,
-    East,
-    South,
-    West,
-}
-
-impl Direction {
-    fn char(&self) -> char {
-        match self {
-            Direction::North => 'N',
-            Direction::East => 'E',
-            Direction::South => 'S',
-            Direction::West => 'W',
-        }
-    }
-
-    fn turn_right(&self) -> Direction {
-        match self {
-            Direction::North => Direction::East,
-            Direction::East => Direction::South,
-            Direction::South => Direction::West,
-            Direction::West => Direction::North,
         }
     }
 }
