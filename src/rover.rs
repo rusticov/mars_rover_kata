1e795381fdf391a9a30d5b30292bc370229dd18b
Rusticov <knightdestruction@pm.me>
2024-12-10 08:01:33 +0000

    feat: Rover can mow forwards in any direction

--- src/rover.rs
+++ src/rover.rs
@@ -1,19 +1,55 @@
 use crate::direction::Direction;
 
+type Coordinate = u8;
+
 pub struct Rover {
     direction: Direction,
+    y: Coordinate,
+    x: Coordinate,
 }
 
 impl Rover {
+    const GRID_LONGITUDE_SIZE: Coordinate = 10;
+    const GRID_LATITUDE_SIZE: Coordinate = 10;
+
     pub fn execute_commands(&mut self, commands: &str) -> String {
         for command in commands.chars() {
             match command {
                 'R' => self.direction = self.direction.turn_right(),
                 'L' => self.direction = self.direction.turn_left(),
+                'M' => match self.direction {
+                    Direction::North => {
+                        self.y += 1;
+                        if self.y == Self::GRID_LONGITUDE_SIZE {
+                            self.y = 0;
+                        }
+                    }
+                    Direction::South => {
+                        self.y = if self.y == 0 {
+                            Self::GRID_LONGITUDE_SIZE - 1
+                        } else {
+                            self.y - 1
+                        }
+                    }
+                    Direction::East => {
+                        self.x += 1;
+                        if self.x == Self::GRID_LATITUDE_SIZE {
+                            self.x = 0;
+                        }
+                    }
+                    Direction::West => {
+                        self.x = if self.x == 0 {
+                            Self::GRID_LATITUDE_SIZE - 1
+                        } else {
+                            self.x - 1
+                        }
+                    }
+                },
                 _ => (),
             }
         }
-        format!("0:0:{}", self.direction.char())
+
+        format!("{}:{}:{}", self.x, self.y, self.direction.char())
     }
 }
 
@@ -21,6 +57,8 @@ impl Rover {
     pub fn new() -> Rover {
         Rover {
             direction: Direction::North,
+            y: 0,
+            x: 0,
         }
     }
 }
--- tests/commands_to_rover_test.rs
+++ tests/commands_to_rover_test.rs
@@ -26,3 +26,23 @@ fn rover_can_turn(commands: &str, expected_final_rover_position: &str) {
 
     pretty_assertions::assert_eq!(rover_end_position, expected_final_rover_position)
 }
+
+#[parameterized(
+    move_north_once = {"M", "0:1:N"},
+    move_north_to_end_of_grid = {"MMMMMMMMM", "0:9:N"},
+    move_north_to_beyond_end_of_grid = {"MMMMMMMMMM", "0:0:N"},
+    move_south_once_wraps_to_top_of_grid = {"LLM", "0:9:S"},
+    move_south_multiple_times = {"LLMM", "0:8:S"},
+    move_east_once = {"RM", "1:0:E"},
+    move_east_to_end_of_grid = {"RMMMMMMMMM", "9:0:E"},
+    move_east_to_beyond_end_of_grid = {"RMMMMMMMMMM", "0:0:E"},
+    move_west_once_wraps_to_top_of_grid = {"LM", "9:0:W"},
+    move_west_multiple_times = {"LMM", "8:0:W"},
+)]
+fn rover_move_forwards(commands: &str, expected_final_rover_position: &str) {
+    let mut rover = Rover::new();
+
+    let rover_end_position = rover.execute_commands(commands);
+
+    pretty_assertions::assert_eq!(rover_end_position, expected_final_rover_position)
+}
