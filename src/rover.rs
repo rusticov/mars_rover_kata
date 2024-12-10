5f754abb56b39d79bdf02fa590d1fc9b9db88f88
Rusticov <knightdestruction@pm.me>
2024-12-10 21:04:55 +0000

    refactor: Move location rules to new Grid struct

--- src/rover.rs
+++ src/rover.rs
@@ -61,50 +61,66 @@ impl Location {
 
 #[derive(Default)]
 pub struct Rover {
+    grid: Grid,
     direction: Direction,
     location: Location,
 }
 
 impl Rover {
-    const GRID_LONGITUDE_SIZE: Coordinate = 10;
-    const GRID_LATITUDE_SIZE: Coordinate = 10;
-
     pub fn execute_commands(&mut self, commands: &str) -> String {
         for command in commands.chars() {
             match command {
                 'R' => self.direction = self.direction.turn_right(),
                 'L' => self.direction = self.direction.turn_left(),
                 'M' => {
-                    self.location = self.location.move_forwards(self.direction);
-                    match self.direction {
-                        Direction::North => {
-                            if self.location.north() == Self::GRID_LONGITUDE_SIZE {
-                                self.location = self.location.teleport_north(0);
-                            }
-                        }
-                        Direction::South => {
-                            if self.location.north() < 0 {
-                                self.location =
-                                    self.location.teleport_north(Self::GRID_LONGITUDE_SIZE - 1);
-                            }
-                        }
-                        Direction::East => {
-                            if self.location.east() == Self::GRID_LATITUDE_SIZE {
-                                self.location = self.location.teleport_east(0);
-                            }
-                        }
-                        Direction::West => {
-                            if self.location.east() < 0 {
-                                self.location =
-                                    self.location.teleport_east(Self::GRID_LATITUDE_SIZE - 1);
-                            }
-                        }
-                    }
+                    let location = self.location.move_forwards(self.direction);
+                    self.location = self.grid.move_to_location(location, self.direction);
                 }
                 _ => (),
             }
         }
 
         format!("{}:{}", self.location.format(":"), self.direction.char())
+    }
+}
+
+#[derive(Default)]
+struct Grid {}
+
+impl Grid {
+    const GRID_LONGITUDE_SIZE: Coordinate = 10;
+    const GRID_LATITUDE_SIZE: Coordinate = 10;
+
+    fn move_to_location(&self, location: Location, direction: Direction) -> Location {
+        match direction {
+            Direction::North => {
+                if location.north() == Self::GRID_LONGITUDE_SIZE {
+                    location.teleport_north(0)
+                } else {
+                    location
+                }
+            }
+            Direction::South => {
+                if location.north() < 0 {
+                    location.teleport_north(Self::GRID_LONGITUDE_SIZE - 1)
+                } else {
+                    location
+                }
+            }
+            Direction::East => {
+                if location.east() == Self::GRID_LATITUDE_SIZE {
+                    location.teleport_east(0)
+                } else {
+                    location
+                }
+            }
+            Direction::West => {
+                if location.east() < 0 {
+                    location.teleport_east(Self::GRID_LATITUDE_SIZE - 1)
+                } else {
+                    location
+                }
+            }
+        }
     }
 }
